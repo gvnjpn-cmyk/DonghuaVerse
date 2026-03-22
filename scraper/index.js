c8d2001be0f15b2028c38964b66774ef8f745c9b
gvnjpn-cmyk <gvnjpn@gmail.com>
2026-03-22 16:00:53 +0700

    Add files via upload

--- scraper/index.js
+++ scraper/index.js
@@ -4,7 +4,9 @@
  */
 
 require("dotenv").config();
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
 const { createClient } = require("@supabase/supabase-js");
 
 const BASE = "https://anichin.cafe";
